d0772978177260a2ade8af4f04df2063786150f1
Mandar Salvi <salvim800@gmail.com>
2020-10-07 13:48:33 +0530

    Complete Cart Activity

--- app/src/main/java/com/example/emandi/cart_activity.java
+++ app/src/main/java/com/example/emandi/cart_activity.java
@@ -2,8 +2,10 @@ package com.example.emandi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,11 +18,14 @@ public class cart_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart_activity);
+        SharedPreferences sharedPreferences = getSharedPreferences(Login.SHARED_PREFS,MODE_PRIVATE);
+        SharedPreferences.Editor editor = editor = sharedPreferences.edit();
         Bundle extra =  getIntent().getBundleExtra("cart");
         ArrayList<Model> list = (ArrayList<Model>) extra.getSerializable("objects");
         TextView totalPrice = findViewById(R.id.totalprice);
         ListView listcart = findViewById(R.id.cartlist);
         int Price = 0;
+
         CartListAdapter adapter = new CartListAdapter(this,R.layout.cart_list,list);
         listcart.setAdapter(adapter);
         for(Model m : list){
@@ -29,10 +34,24 @@ public class cart_activity extends AppCompatActivity {
         //Toast.makeText(this,Integer.toString(adapter.totalprice),Toast.LENGTH_LONG).show();
         totalPrice.setText(Integer.toString(Price)+" Rs");
 
+        String address = sharedPreferences.getString("add","");
+        TextView add = findViewById(R.id.addresscart);
+        add.setText(address);
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
     }
 
     @Override
     public void onBackPressed() {
+        finish();
+    }
+
+    public void Checkout(View view) {
 
     }
 }
