7b754c9025ec5bc68ff8969e0ba627e6587e55a1
Mandar Salvi <salvim800@gmail.com>
2020-10-08 21:12:27 +0530

    Profile Layout Design

--- app/src/main/java/com/example/emandi/Profile_activity.java
+++ app/src/main/java/com/example/emandi/Profile_activity.java
@@ -2,14 +2,34 @@ package com.example.emandi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Profile_activity extends AppCompatActivity {
-
+    SharedPreferences sharedPreferences ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_activity);
+        sharedPreferences = getSharedPreferences(Login.SHARED_PREFS,MODE_PRIVATE);
+        TextView name = findViewById(R.id.cartName);
+        TextView mail = findViewById(R.id.cartmail);
+        TextView num = findViewById(R.id.cartmobile);
+        TextView add = findViewById(R.id.addresscart);
+        name.setText(sharedPreferences.getString("un",""));
+        mail.setText(sharedPreferences.getString("email",""));
+        num.setText(sharedPreferences.getString("no",""));
+        add.setText(sharedPreferences.getString("add",""));
+
+
+
+
+
+
+
     }
     public void onBackPressed() {
         finish();
